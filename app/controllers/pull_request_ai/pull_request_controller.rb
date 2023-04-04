aeab35d9c9f31c87ad09964c9c6c9618b79266f0
Filipe Martins <>
2023-04-04 17:48:13 +0100

    implement UI iteration to prepare the pull request

--- app/controllers/pull_request_ai/pull_request_controller.rb
+++
@@ -1,9 +0,0 @@
-require_dependency 'pull_request_ai/application_controller'
-
-module PullRequestAi
-  class PullRequestController < ApplicationController
-    # default page render
-    def index
-    end
-  end
-end
