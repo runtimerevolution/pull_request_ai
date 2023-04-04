9afdb6b5370c97806987af37aa90be0eee9986c8
André Franco <andre.gfranco@gmail.com>
2023-04-04 09:57:28 +0100

    engine_setup: Remove sqlite3 and active storage methods

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -1,6 +1,9 @@
 require_relative "boot"
 
-require "rails/all"
+require 'action_controller/railtie'
+require 'action_view/railtie'
+require 'sprockets/railtie'
+require 'rails/engine/commands'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
