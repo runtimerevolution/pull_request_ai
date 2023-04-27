91ca3868c3319df9152a80aaa2e0fa33b64eabeb
Filipe Martins <>
2023-04-27 16:19:15 +0100

    move unit tests to the client part

--- spec/pull_request_ai/open_ai/client_spec.rb
+++ spec/pull_request_ai/open_ai/client_spec.rb
@@ -60,15 +60,60 @@ RSpec.describe(PullRequestAi::OpenAi::Client) do
   end
 
   describe '.request' do
-    before do
-      allow(HTTParty).to(receive(:send).and_return(fake_http_response))
-      allow(fake_http_response).to(receive(:parsed_response)).and_return({})
-      allow(fake_http_response).to(receive(:success?)).and_return(true)
+    context 'with invalid request' do
+      # Invalid Request
+      let(:error_body) do
+        {
+          'error' =>
+                { 'message' => "You didn't provide an API key.", 'type' => 'invalid_request_error' }
+        }
+      end
+      let(:invalid_http_response) do
+        instance_double(
+          HTTParty::Response,
+          code: 401,
+          parsed_response: error_body,
+          success?: false
+        )
+      end
+
+      before { allow(HTTParty).to(receive(:post).and_return(invalid_http_response)) }
+      
+      it 'builds and returns an invalid response object' do
+          response = client.predicted_completions(content: 'new chat')
+
+          expect(HTTParty).to(have_received(:post).with(any_args))
+          expect(response).to(be_failure)
+          expect(response).not_to(be_success)
+          expect(response.failure.symbol).to(eq(:failed_on_openai_api_endpoint))
+          expect(response.failure.message).to(eq(error_body.dig('error', 'message')))
+        end
     end
 
-    it 'sends user content to httparty request' do
-      client.predicted_completions(content: 'new chat')
-      expect(HTTParty).to(have_received(:send).with(any_args))
+    context 'with valid request' do
+      # Valid Request
+      let(:success_body) do
+        { 'choices' => [{ 'message' => { 'role' => 'assistant', 'content' => 'your PR' } }] }
+      end
+      let(:valid_http_response) do
+        instance_double(
+          HTTParty::Response,
+          code: 200,
+          parsed_response: success_body,
+          success?: true
+        )
+      end
+
+      before { allow(HTTParty).to(receive(:post).and_return(valid_http_response)) }
+
+      it 'builds and returns a valid response object' do
+        response = client.predicted_completions(content: 'new chat')
+
+        expect(HTTParty).to(have_received(:post).with(any_args))
+        expect(response).to(be_success)
+        expect(response).not_to(be_failure)
+        expect(response.success).to(eq(success_body['choices'].first.dig('message', 'content')))
+      end
     end
   end
 end
